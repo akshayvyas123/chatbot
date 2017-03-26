902eabc20f6bb0f59541ceb8ec7370fd39ccda67
varunrastogi <varunrastogi2007@gmail.com>
2017-03-26 09:10:50 +0530

    init

--- index.js
+++ index.js
@@ -93,9 +93,9 @@ var datajsonform=JSON.stringify(data);
  // };
    // res.write(JSON.stringify(responseBody));
    // res.end();
-var response1=JSON.parse(response)
+var response1=JSON.parse(response.body)
 var flightsava="flightno:"
-+response1.body.segment[0].flights[0].lfId +" departure time :" +response1.body.segment[0].flights[0].stops.departureTime+"departure time :"+ response1.body.segment[0].flights[0].stops.arrivalTime;
++response1.segment[0].flights[0].lfId +" departure time :" +response1.segment[0].flights[0].stops.departureTime+"departure time :"+ response1.segment[0].flights[0].stops.arrivalTime;
 
 var json = JSON.stringify({
     data:{
